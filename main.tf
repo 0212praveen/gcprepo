a030e5ff9e96986ca009b124e91e579c01fb93aa
0212praveen <166958499+0212praveen@users.noreply.github.com>
2024-04-13 17:18:37 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  project = "fiery-bit-420221"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = "my-praveen-bucket-name"
+  location      = "US"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    log_bucket = "logs_bucket"
+  }
+
+  lifecycle {
+    rule {
+      action {
+        type = "Delete"
+      }
+      condition {
+        age = 30
+      }
+    }
+  }
+}
